0a343ba0ee188678ae359fe61ddd8e17f7fcd09a
JhonnyTest259 <jhobatrix@gmail.com>
2022-06-10 16:50:22 -0500

    actualFecha

--- consultorio/lib/main.dart
+++ consultorio/lib/main.dart
@@ -14,10 +14,12 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
